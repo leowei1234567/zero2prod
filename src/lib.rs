c5b4aa0ffcce287b6933b31669b37d83f9807e75
jixing <13770935170@163.com>
2022-09-15 00:52:09 -0700

    random port

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health", web::get().to(health_check)))
-        .bind("0.0.0.0:8090")?
+        .listen(listener)?
         .run();
     Ok(server)
 }
