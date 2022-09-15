e418a90811991a76a9686c813e357454eb7e6854
jixing <13770935170@163.com>
2022-09-14 20:46:07 -0700

    first test

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/health", web::get().to(health_check)))
+        .bind("0.0.0.0:8090")?
+        .run()
+        .await
+}
