0a1073a6d0ca87f97e116ab77152d4aaf9f09598
jixing <13770935170@163.com>
2022-09-15 00:37:17 -0700

    fix intergration test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,20 @@
-use zero2prod::main;
-#[test]
-fn dummy_test() {
-main()
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8090/health")
+        .send()
+        .await
+        .expect("Failed to excute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
 }
