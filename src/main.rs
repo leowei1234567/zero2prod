602efddc0d1b4c309cb7c7e1aa4dc273fc1097a2
jixing <13770935170@163.com>
2022-09-20 02:14:06 -0700

    refine code

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,14 @@ use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to get config");
+
     let address = format!("0.0.0.0:{}", configuration.application_port);
-    let connection_string = configuration.database.connection_string();
-    let connection_pool = PgPool::connect(&connection_string)
+
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
+
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
+
     run(listener, connection_pool)?.await
 }
