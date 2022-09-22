bcb25ce601923144cb8bbb639842d3aaaf48e7b0
jixing <13770935170@163.com>
2022-09-22 01:32:14 -0700

    before docker

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,19 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use secrecy::ExposeSecret;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to get config");
 
     let address = format!("0.0.0.0:{}", configuration.application_port);
 
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
 
