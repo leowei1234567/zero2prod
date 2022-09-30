e921688f16932cd46bf006e42feda54aec647bb6
jixing <13770935170@163.com>
2022-09-29 19:12:23 -0700

    refine test

---
+++ tests/api/health_check.rs
@@ -0,0 +1,17 @@
+use crate::helpers::spawn_app;
+
+#[tokio::test]
+async fn health_check_works() {
+    let test_app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let url = format!("{}/health", test_app.address);
+    let response = client
+        .get(url)
+        .send()
+        .await
+        .expect("Failed to excute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
